5f252b1479eb220e7a8b37484b246926c80c3f4c
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-05-20 14:30:04 +0600

    api for delete operation creatd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,11 +32,24 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/allToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.findOne(query);
+      res.send(result);
+    });
 
     app.post("/addToy", async (req, res) => {
       const toy = req.body;
       console.log(toy);
       const result = await toysCollection.insertOne(toy);
+      res.send(result);
+    });
+
+    app.delete("/allToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.deleteOne(query);
       res.send(result);
     });
 
